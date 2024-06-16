bcb23bbed6f237ee509010e1e402644a393a0c08
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 15:00:51 -0500

    debug

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var map = L.map('mapid').setView([20, 0], 2);
+/*map of geojson data from MegaCities.geojson*/
+
+var map;
+
+function createMap(){
+    map = L.map('map', {
+        center: [20,0],
+        zoom: 2
+    });
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
+    GamepadHapticActuator();
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
     fetch("data/MegaCities.geojson")
-        .then(response => response.json())
-        .then(json => {
-            L.geoJSON(json, {
-                pointToLayer: function(feature, latlng) {
-                    return L.circleMarker(latlng, {
-                        radius: 8,
-                        fillColor: "#ff7800",
-                        color: "#000",
-                        weight: 1,
-                        opacity: 1,
-                        fillOpacity: 0.8
-                    });
-                },
-                onEachFeature: function(feature, layer) {
-                    var popupContent = "<p><strong>City:</strong> " + feature.properties.City + "</p>";
-                    popupContent += "<p><strong>Population 2015:</strong> " + feature.properties.Pop_2015 + " million</p>";
-                    layer.bindPopup(popupContent);
-                }
-            }).addTo(map);
-        });
-});
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            L.geoJSON(json).addTo(map);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
