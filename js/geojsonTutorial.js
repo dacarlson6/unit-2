6f9950dca574a1c241ec43ad608ab467b711f76a
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 14:13:08 -0500

    debug geojson

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,10 +1,4 @@
-// var map = L.map('mapid').setView([39.75621, -104.99404], 13);
-
-function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
+var map = L.map('mapid').setView([39.75621, -104.99404], 13);
 
 var geojsonFeature = {
     "type": "Feature",
@@ -19,65 +13,6 @@ var geojsonFeature = {
     }
 };
 
-L.geoJSON(geojsonFeature, {
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
-
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-L.geoJSON(myLines, {
-    style: myStyle
-}).addTo(map);
-
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
-
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
-        }
-    }
-}).addTo(map);
-
 var geojsonMarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
@@ -87,36 +22,48 @@ var geojsonMarkerOptions = {
     fillOpacity: 0.8
 };
 
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
 L.geoJSON(geojsonFeature, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
+    },
+    onEachFeature: onEachFeature
 }).addTo(map);
 
-var someFeatures = [{
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "show_on_map": true
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-}, {
-    "type": "Feature",
-    "properties": {
-        "name": "Busch Field",
-        "show_on_map": false
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.98404, 39.74621]
-    }
-}];
+// Adding line and polygon features with styles
+var myLines = [
+    {"type": "LineString", "coordinates": [[-100, 40], [-105, 45], [-110, 55]]},
+    {"type": "LineString", "coordinates": [[-105, 40], [-110, 45], [-115, 55]]}
+];
 
-L.geoJSON(someFeatures, {
-    filter: function(feature, layer) {
-        return feature.properties.show_on_map;
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, { style: myStyle }).addTo(map);
+
+var states = [
+    {
+        "type": "Feature",
+        "properties": {"party": "Republican"},
+        "geometry": {"type": "Polygon", "coordinates": [[[-104.05, 48.99], [-97.22, 48.98], [-96.58, 45.94], [-104.03, 45.94], [-104.05, 48.99]]]}
+    },
+    {
+        "type": "Feature",
+        "properties": {"party": "Democrat"},
+        "geometry": {"type": "Polygon", "coordinates": [[[-109.05, 41.00], [-102.06, 40.99], [-102.03, 36.99], [-109.04, 36.99], [-109.05, 41.00]]]}
+    }
+];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        return feature.properties.party === 'Republican' ? {color: "#ff0000"} : {color: "#0000ff"};
     }
 }).addTo(map);
