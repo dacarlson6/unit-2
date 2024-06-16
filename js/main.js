29bcc15422525e56faf4d1c65242aba8ca1bee32
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 12:07:17 -0500

    debug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-//Example 1.1 line 5...add tile layer
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Initialize the map on the "mapid" div with a given center and zoom
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Changed http to https
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map);
