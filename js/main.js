5c82a6efd652ad4112c75f2a1168d3d8e98321e7
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 11:35:59 -0500

    added script to main

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-// Add all scripts to the JS folder
+var map = L.map('mapid').setView([latitude, longitude], zoomLevel);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
