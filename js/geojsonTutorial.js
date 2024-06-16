7386f432104e1763b2c6e1aac590947a5f7090be
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 14:15:19 -0500

    geojson debug

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,4 +1,8 @@
-var map = L.map('mapid').setView([39.75621, -104.99404], 13);
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
 
 var geojsonFeature = {
     "type": "Feature",
@@ -13,6 +17,65 @@ var geojsonFeature = {
     }
 };
 
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
 var geojsonMarkerOptions = {
     radius: 8,
     fillColor: "#ff7800",
@@ -22,48 +85,36 @@ var geojsonMarkerOptions = {
     fillOpacity: 0.8
 };
 
-function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
-}
-
 L.geoJSON(geojsonFeature, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
-    },
-    onEachFeature: onEachFeature
+    }
 }).addTo(map);
 
-// Adding line and polygon features with styles
-var myLines = [
-    {"type": "LineString", "coordinates": [[-100, 40], [-105, 45], [-110, 55]]},
-    {"type": "LineString", "coordinates": [[-105, 40], [-110, 45], [-115, 55]]}
-];
-
-var myStyle = {
-    "color": "#ff7800",
-    "weight": 5,
-    "opacity": 0.65
-};
-
-L.geoJSON(myLines, { style: myStyle }).addTo(map);
-
-var states = [
-    {
-        "type": "Feature",
-        "properties": {"party": "Republican"},
-        "geometry": {"type": "Polygon", "coordinates": [[[-104.05, 48.99], [-97.22, 48.98], [-96.58, 45.94], [-104.03, 45.94], [-104.05, 48.99]]]}
+var someFeatures = [{
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "show_on_map": true
     },
-    {
-        "type": "Feature",
-        "properties": {"party": "Democrat"},
-        "geometry": {"type": "Polygon", "coordinates": [[[-109.05, 41.00], [-102.06, 40.99], [-102.03, 36.99], [-109.04, 36.99], [-109.05, 41.00]]]}
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
     }
-];
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Busch Field",
+        "show_on_map": false
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.98404, 39.74621]
+    }
+}];
 
-L.geoJSON(states, {
-    style: function(feature) {
-        return feature.properties.party === 'Republican' ? {color: "#ff0000"} : {color: "#0000ff"};
+L.geoJSON(someFeatures, {
+    filter: function(feature, layer) {
+        return feature.properties.show_on_map;
     }
 }).addTo(map);
