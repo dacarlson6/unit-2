efdfd5ba20a5e8f83c7844e31e22105a921958d8
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-16 13:10:52 -0500

    debugging geojson

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-// Initialize the map on the "mapid" div with a given center and zoom
+/* // Initialize the map on the "mapid" div with a given center and zoom
 var map = L.map('mapid').setView([51.505, -0.09], 13);
 
 // Add tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Changed http to https
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map); */
+
+var map = L.map('mapid').setView([39.75621, -104.99404], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    maxZoom: 18
 }).addTo(map);
