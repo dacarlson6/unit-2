d2d034a6e2460cc676ec6f58b410645c4110a756
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-17 17:29:14 -0500

    water levels

--- js/main.js
+++ js/main.js
@@ -6,18 +6,62 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Changed h
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 }).addTo(map); */
 
-var map = L.map('mapid').setView([20, 0], 3);
+document.addEventListener('DOMContentLoaded',function() {
+    createMap();
+});
+
+function createMap(){
+    var map = L.map('mapid', {
+        center: [20,0],
+        zoom: 2
+    });
+
+// var map = L.map('mapid').setView([20, 0], 3);
 
 /* L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
     maxZoom: 18 */
 
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-    maxZoom: 13
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+        maxZoom: 13
+    }).addTo(map);
 
-}).addTo(map);
+    getData(map);
+}
 
-/* var Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	maxZoom: 13 */
+//function to retrieve the data and place it on the map
+function getData(map){
+    //load the data
+    fetch("data/WaterLevels.geojson")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            L.geoJSON(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                onEachFeature: function(feature, layer) {
+                    var popupContent = "";
+                    if(feature.properties) {
+                        //loop to add feature property names and values to html string
+                        for (var property in feature.properties) {
+                            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+                        }
+                        layer.bindPopup(popupContent);
+                    }
+                }
+            }).addTo(map);
+        });
+}
