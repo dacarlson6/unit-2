c94d889cdb11cb7a6ede2630447919d5d98c39e7
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-17 17:36:36 -0500

    debug zoom

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded',function() {
 
 function createMap(){
     var map = L.map('mapid', {
-        center: [45,-85],
+        center: [45,0],
         zoom: 6
     });
 
