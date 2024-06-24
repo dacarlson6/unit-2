cafd8170c101076eacf6f326681ccab46e323af1
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-06-23 19:54:57 -0500

    slider

--- js/main.js
+++ js/main.js
@@ -1,27 +1,12 @@
-/* // Initialize the map on the "mapid" div with a given center and zoom
-var map = L.map('mapid').setView([51.505, -0.09], 13);
-
-// Add tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Changed http to https
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-}).addTo(map); */
 
 var map;
 var minValue;
-
-/* document.addEventListener('DOMContentLoaded',function() {
-    createMap();
-}); */
 
 function createMap(){
     map = L.map('mapid', {
         center: [44.5, -85.0],
         zoom: 6
     });
-
-/* L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
-    maxZoom: 18 */
 
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
@@ -103,21 +88,13 @@ function createPropSymbols(data) {
     }).addTo(map);
 };
 
-
-
-/*     L.geoJSON(data, {
-        pointToLayer: function(feature, latlng) {
-            //step 5: for each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            //step 6: give each feature's circle marker a radius based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-    }).addTo(map);
-}; */
+//create new sequence controls
+function createSequenceControls(data) {
+    
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend', slider);
+};
 
 //function to retrieve the data and place it on the map
 function getData(map){
@@ -131,39 +108,8 @@ function getData(map){
             minValue = calculateMinValue(json);
             //call function to create proportional symbols
             createPropSymbols(json);
+            createSequenceControls();
         });
 }
 
 document.addEventListener('DOMContentLoaded', createMap);
-            
-            
-            
-            
-            
-            /* //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#0077be",
-                color: "#005a9c",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.7
-            };
-
-            L.geoJSON(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                },
-                onEachFeature: function(feature, layer) {
-                    var popupContent = "<h3>Highest Water Level Reported by Station per Month</h3>";
-                    if(feature.properties) {
-                        //loop to add feature property names and values to html string
-                        for (var property in feature.properties) {
-                            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-                        }
-                        layer.bindPopup(popupContent);
-                    }
-                }
-            }).addTo(map);
-        });
-} */
